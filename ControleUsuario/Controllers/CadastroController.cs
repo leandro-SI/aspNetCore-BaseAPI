4cf491baefe4253f0642ea28e2fbe46f0872b3fe
Leandro <leandro.almeida@uvvnet.com.br>
2022-06-12 15:09:33 -0300

    escopo para controle de usuário

---
+++ ControleUsuario/Controllers/CadastroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleUsuario.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CadastroController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult CadastroUsuario()
+        {
+            // service
+
+            return Ok();
+        }
+    }
+}
---
+++ ControleUsuario/Profiles/UsuarioProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ControleUsuario.Data.Dtos.UsuarioDto;
+using ControleUsuario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleUsuario.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
