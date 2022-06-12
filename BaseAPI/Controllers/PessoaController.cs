9952755bc2c774088407ec87d99c8cb9e2b7e52d
Leandro <leandro.almeida@uvvnet.com.br>
2022-06-11 23:58:26 -0300

    endpoint básico para as requisições

--- BaseAPI/Controllers/PessoaController.cs
+++ BaseAPI/Controllers/PessoaController.cs
@@ -1,4 +1,5 @@
-﻿using BaseAPI.Models;
+﻿using BaseAPI.Data;
+using BaseAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,11 +12,65 @@ namespace BaseAPI.Controllers
     [Route("[controller]")]
     public class PessoaController : ControllerBase
     {
+        private readonly BaseContext _context;
+
+        public PessoaController(BaseContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
-        public void AdicionarPessoa([FromBody] Pessoa pessoa)
+        public IActionResult AdicionarPessoa([FromBody] Pessoa pessoa)
         {
-            // lógica
+            _context.Pessoas.Add(pessoa);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperarPessoaPorId), new { Id = pessoa.Id }, pessoa);
         }
+
+        [HttpGet]
+        public IActionResult RecuperarPessoas()
+        {
+            return Ok(_context.Pessoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarPessoaPorId(int id)
+        {
+            Pessoa pessoa = _context.Pessoas.FirstOrDefault(filme => filme.Id == id);
+
+            if (pessoa != null) return Ok(pessoa);
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarPessoa([FromBody] Pessoa pessoaRequest, int id)
+        {
+            var pessoa = _context.Pessoas.FirstOrDefault(p => p.Id == id);
+
+            if (pessoa == null) return NotFound();
+
+            pessoa.Nome = pessoaRequest.Nome;
+            pessoa.Sobrenome = pessoaRequest.Sobrenome;
+            pessoa.Idade = pessoaRequest.Idade;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarPessoa(int id)
+        {
+            var pessoa = _context.Pessoas.FirstOrDefault(p => p.Id == id);
+
+            if (pessoa == null) return NotFound();
+            _context.Remove(pessoa);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
