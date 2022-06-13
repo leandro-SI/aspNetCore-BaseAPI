3df88d17826d7b5e21a19e9983023147f3cb0848
Leandro <leandro.almeida@uvvnet.com.br>
2022-06-12 22:28:38 -0300

    gerando token para autenticação

---
+++ ControleUsuario/Models/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleUsuario.Models
+{
+    public class Token
+    {
+        public string Value { get; }
+
+        public Token(string value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ ControleUsuario/Services/TokenService.cs
@@ -0,0 +1,39 @@
+﻿using ControleUsuario.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleUsuario.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> user)
+        {
+            Claim[] direitosUser = new Claim[]
+            {
+                new Claim("username", user.UserName),
+                new Claim("id", user.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("lfli803nvnkw9302"));
+
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: direitosUser,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddHours(1));
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new Token(tokenString);
+
+        }
+    }
+}
